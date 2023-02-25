309eb90c73980a52e62f053ef9581e34f52f09f7
Re4GD <57919250+Re4GD@users.noreply.github.com>
2023-02-25 22:28:45 +0300

    fix: renamed secret

--- src/webhook.ts
+++ src/webhook.ts
@@ -4,13 +4,13 @@ import { VerifyWebhookResult, WebhookBody } from "./types/webhook";
 export const verifyWebhook = (
 	rawBody: unknown,
 	signature: string | string[] | undefined,
-	sharedSecret: string,
+	ipnSecret: string,
 ): VerifyWebhookResult => {
 	if (!signature) {
 		return { isVerified: false, rawBody: rawBody, error: "NO_SIGNATURE" };
 	}
 	const coinBaseSignature = crypto
-		.createHmac("sha256", sharedSecret)
+		.createHmac("sha256", ipnSecret)
 		.update(JSON.stringify(rawBody))
 		.digest("hex");
 	if (signature !== coinBaseSignature) {
