bc88eb01b4719fbcbbd06c26c96342cbb28aab95
Re4GD <57919250+Re4GD@users.noreply.github.com>
2023-02-25 23:02:03 +0300

    fix: signature

--- src/webhook.ts
+++ src/webhook.ts
@@ -2,18 +2,18 @@ import crypto from "crypto";
 import { VerifyWebhookResult, WebhookBody } from "./types/webhook";
 
 export const verifyWebhook = (
-	rawBody: object,
+	rawBody: any,
 	signature: string | string[] | undefined,
 	ipnSecret: string,
 ): VerifyWebhookResult => {
 	if (!signature) {
 		return { isVerified: false, rawBody: rawBody, error: "NO_SIGNATURE" };
 	}
-	const coinBaseSignature = crypto
+	const npSignature = crypto
 		.createHmac("sha512", ipnSecret)
 		.update(JSON.stringify(rawBody, Object.keys(rawBody).sort()))
 		.digest("hex");
-	if (signature !== coinBaseSignature) {
+	if (signature !== npSignature) {
 		return { isVerified: false, rawBody: rawBody, error: "INVALID_SIGNATURE" };
 	}
 	return {
