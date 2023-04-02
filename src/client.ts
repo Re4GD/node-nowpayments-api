6cef47a370dd175264d4d389888a933b32c58d6e
Re4GD <57919250+Re4GD@users.noreply.github.com>
2023-04-02 14:03:29 +0300

    feat: added payments payouts api endpoints

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,25 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import {
+	CreatePaymentParams,
+	CreatePaymentResult,
+	CreatePayoutParams,
+	CreatePayoutResult,
+	GetApiStatusResult,
 	GetAvailableCheckedCoins,
 	GetAvailableCheckedCoinsParams,
 	GetAvailableCurrencies,
 	GetAvailableCurrenciesParams,
 	GetAvailableFullCurrencies,
+	GetBalanceResult,
+	GetEstimatedPriceParams,
+	GetEstimatedPriceResult,
+	GetPayoutStatusParams,
+	GetPayoutStatusResult,
+	PayoutAuthenticationParams,
+	PayoutAuthenticationResult,
 	Result,
+	VerifyPayoutParams,
+	VerifyPayoutResult,
 } from "./types";
 
 export class NowPaymentsClient {
@@ -23,6 +37,15 @@ export class NowPaymentsClient {
 			...requestOptions,
 		};
 		this.axiosInstance = axios.create(this.globalRequestOptions);
+	}
+
+	public async getApiStatus(): Promise<Result<GetApiStatusResult>> {
+		try {
+			const { data } = await this.axiosInstance.get("/v1/status");
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
 	}
 
 	public async getAvailableCurrencies(
@@ -60,6 +83,107 @@ export class NowPaymentsClient {
 					fixed_rate: params.fixed_rate,
 				},
 			});
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
+	}
+
+	public async getEstimatedPrice(
+		params: GetEstimatedPriceParams,
+	): Promise<Result<GetEstimatedPriceResult>> {
+		try {
+			const { data } = await this.axiosInstance.get("/v1/estimate", {
+				params: {
+					amount: params.amount,
+					currency_from: params.currency_from,
+					currency_to: params.currency_to,
+				},
+			});
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
+	}
+
+	public async createPayment(
+		params: CreatePaymentParams,
+	): Promise<Result<CreatePaymentResult>> {
+		try {
+			const { data } = await this.axiosInstance.post("/v1/payment", params);
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
+	}
+
+	public async payoutAuthentication(
+		params: PayoutAuthenticationParams,
+	): Promise<Result<PayoutAuthenticationResult>> {
+		try {
+			const { data } = await this.axiosInstance.post("/v1/auth", params);
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
+	}
+
+	public async getBalance(): Promise<Result<GetBalanceResult>> {
+		try {
+			const { data } = await this.axiosInstance.get("/v1/balance");
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
+	}
+
+	public async getPayoutStatus(
+		params: GetPayoutStatusParams,
+	): Promise<Result<GetPayoutStatusResult>> {
+		try {
+			const { data } = await this.axiosInstance.get(
+				`/v1/payout/${params.payout_id}`,
+			);
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
+	}
+
+	public async createPayout(
+		params: CreatePayoutParams,
+		authorization: string,
+	): Promise<Result<CreatePayoutResult>> {
+		const jwtToken = "Bearer " + authorization.replace("Bearer ", "");
+		try {
+			const { data } = await this.axiosInstance.post("/v1/payout", params, {
+				headers: {
+					Authorization: jwtToken,
+				},
+			});
+			return { result: data };
+		} catch (error) {
+			return { error: error };
+		}
+	}
+
+	public async verifyPayout(
+		params: VerifyPayoutParams,
+		authorization: string,
+	): Promise<Result<VerifyPayoutResult>> {
+		const jwtToken = "Bearer " + authorization.replace("Bearer ", "");
+		try {
+			const { data } = await this.axiosInstance.post(
+				`/v1/payout/${params.payout_id}/verify`,
+				{
+					verification_code: params.verification_code,
+				},
+				{
+					headers: {
+						Authorization: jwtToken,
+					},
+				},
+			);
 			return { result: data };
 		} catch (error) {
 			return { error: error };
